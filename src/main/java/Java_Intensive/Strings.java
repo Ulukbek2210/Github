2a59662b535d3499d9f51e88f5ea624ab74a0f28
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-20 15:08:25 -0700

    commit

---
+++ src/main/java/Java_Intensive/Strings.java
@@ -0,0 +1,16 @@
+package Java_Intensive;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String s = "String";
+        char[] c = new char[] {'s', 't', 'r', 'i', 'n', 'g',};
+
+        for(int i = 0; i < c.length; ++i); {
+            System.out.println(c);
+        }
+        for(int i = 0; i < s.length(); ++i) {
+        System.out.println(s.charAt(i));
+    }
+}
+}
