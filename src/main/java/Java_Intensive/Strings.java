b8c3f96e8594e569a4dcf5a884d1518a24847224
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-24 19:16:00 -0700

    commit

--- src/main/java/Java_Intensive/Strings.java
+++ src/main/java/Java_Intensive/Strings.java
@@ -3,14 +3,17 @@ package Java_Intensive;
 public class Strings {
 
     public static void main(String[] args) {
-        String s = "String";
-        char[] c = new char[] {'s', 't', 'r', 'i', 'n', 'g',};
+        String s = "    String    y";
+        char[] c = new char[] {'A', 'F', 'T', 'U', 'O', 'G',};
 
         for(int i = 0; i < c.length; ++i); {
             System.out.println(c);
         }
         for(int i = 0; i < s.length(); ++i) {
-        System.out.println(s.charAt(i));
+        System.out.print(s.charAt(i));
     }
+        s = s + " WASSSSAAAP";
+        System.out.println(s);
+
 }
 }
