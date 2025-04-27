0c90f546267760320683b8245ee61d3cadb358bb
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-27 09:17:37 -0700

    commit

--- src/main/java/Java_Intensive/Addition.java
+++ src/main/java/Java_Intensive/Addition.java
@@ -1,7 +1,14 @@
 package Java_Intensive;
 
+import java.sql.SQLOutput;
+
 public class Addition {
     public static int sum(int a, int b) {
+        return a+b;
+
+    }
+
+    public static double sum (double a, double b) {
         return a+b;
 
     }
@@ -9,9 +16,14 @@ public class Addition {
         int a = 1;
         int b = 2;
         int c = a+b;
+        double r=2.5;
+        double t= 3.3;
+        double y = r+t;
 
-        System.out.println();
 
+        System.out.println(c);
+        System.out.println(y);
 
     }
 }
+
