5f5e29fc9b6f7adaa81fc27f88d0bff48157b126
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-29 05:34:28 -0700

    commit

---
+++ src/main/java/Strings/StringAppend.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.sql.SQLOutput;
+
+public class StringAppend {
+    public static void main (String[] args){
+        String s = "String";
+        s += " String";
+        s = "new " + s;
+        System.out.println(s);
+
+    }
+}
