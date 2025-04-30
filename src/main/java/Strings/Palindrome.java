79e85403a6e9c125a96e2f0ebbfa8ec7c5b4032f
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-30 07:35:12 -0700

    commit

--- src/main/java/Strings/Palindrome.java
+++ src/main/java/Strings/Palindrome.java
@@ -3,17 +3,25 @@ package Strings;
 import java.util.Scanner;
 
 public class Palindrome {
+    public static String reverse(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; --i) {
+            char c = s.charAt(i);
+            reversed = reversed + c;
+
+        }
+        return reversed;
+    }
     public static void main(String[] args) {
         // "Palindrome"
         // "emord...."
 
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        String reversed = "";
-        for(int i = s.length()-1; i>=0; --i ) {
-            reversed +=  s.charAt(i);
+        String reversed = reverse(s);
 
-        }
+
+
         if(s.equals(reversed)) {
             System.out.println("Palindrome");
 
