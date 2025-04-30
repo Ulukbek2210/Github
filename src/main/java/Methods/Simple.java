c99ebeafe2262c4ea0a305301e91d17b5b383dcf
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-29 23:25:04 -0700

    commit

---
+++ src/main/java/Methods/Simple.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class Simple {
+    public static void greet(){
+        System.out.println("Hello World");
+
+
+    }
+    public static void main(String[] args) {
+        greet();
+        greet();
+        greet();
+
+
+
+    }
+
+}
