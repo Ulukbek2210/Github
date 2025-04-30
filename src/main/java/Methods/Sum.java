9cd587ce6cd38b8eb1596fe1a7ad4ffd5ec9b185
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-04-29 23:40:15 -0700

    commit

---
+++ src/main/java/Methods/Sum.java
@@ -0,0 +1,17 @@
+package Methods;
+
+public class Sum {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static double sum(double a, double b) {
+        return a +b;
+
+    }
+    public static void main(String[] args){
+        System.out.println(sum(1.5,2));
+        System.out.println(sum(1,2));
+
+    }
+
+}
