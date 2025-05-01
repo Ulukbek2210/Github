de913ab4a6d3dc5083fae48d89090e21dee82c4e
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-05-01 06:01:43 -0700

    commit

---
+++ src/main/java/Week4/Dog.java
@@ -0,0 +1,10 @@
+package Week4;
+
+public class Dog {
+    public String name;
+
+    public void bark(){
+        System.out.println(this.name + "barked!");
+
+    }
+}
---
+++ src/main/java/Week4/Main.java
@@ -0,0 +1,18 @@
+package Week4;
+
+public class Main {
+    public static void main(String[] args){
+        Dog Bobby = new Dog();
+        Bobby.name = "Bobby ";
+
+        Bobby.bark();
+
+
+        Dog buddy = new Dog();
+        buddy.name = "Buddy ";
+
+        buddy.bark();
+
+    }
+
+}
