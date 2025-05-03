62b8b35a1b3f6a5501bb44254006344182c51a6d
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-05-03 13:21:24 -0700

    commit

---
+++ src/main/java/bank/CheckingAccount.java
@@ -0,0 +1,24 @@
+package bank;
+
+public class CheckingAccount {
+    public double balance;
+
+    public CheckingAccount(double balance) {
+        this.balance = balance;
+
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+
+    }
+
+    public double withdraw(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+
+        }
+        return amount;
+    }
+}
+
---
+++ src/main/java/bank/Main.java
@@ -0,0 +1,13 @@
+package bank;
+
+public class Main {
+    public static void main(String[] args){
+        CheckingAccount myAccount = new CheckingAccount(0);
+
+        myAccount.deposit((50));
+        //...
+        myAccount.withdraw(20);
+        //...
+        System.out.println(myAccount.balance);
+    }
+}
