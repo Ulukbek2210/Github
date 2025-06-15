d105cc56c14c952a92a8e7c67db4cd1029715a93
ulukbek2210 <ulukbekgrowthhungry@gmail.com>
2025-06-14 17:10:55 -0700

    commit

--- src/main/java/Week4/chess/Main.java
+++ src/main/java/Week4/chess/Main.java
@@ -2,10 +2,10 @@ package Week4.chess;
 
 public class Main {
     public static void main(String[] args) {
-        Pawn p1 = new Pawn(2,2, "white");
-        Pawn p2 = new Pawn(3,3,"black");
+        Piece p1 = new Knight(2,1, "white");
+        Piece p2 = new Pawn(3,3,"black");
 
         p1.beat(p2.getX(), p2.getY());
-        System.out.println(p1.toString());
+        System.out.println(p1);
     }
 }
--- src/main/java/Week4/chess/Pawn.java
+++ src/main/java/Week4/chess/Pawn.java
@@ -1,43 +1,14 @@
 package Week4.chess;
 
-public class Pawn {
-    private int x, y;
-    private String color;
-
+public class Pawn  extends  Piece{
     public Pawn(int x, int y, String color) {
         this.x = x;
         this.y = y;
         this.color = color;
     }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
     @Override
     public String toString() {
         return "x = " + this.x + "; " + "y = " + this.y;
-    }
-
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public boolean canMove(int nx, int ny) {
